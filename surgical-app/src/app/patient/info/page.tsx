3ed76798b4b9f48ea752df41e8e6195fda47b6e1
Lisa Chan <lc131715@gmail.com>
2025-08-03 16:07:01 -0700

    add styling to patient info screen

--- surgical-app/src/app/patient/info/page.tsx
+++ surgical-app/src/app/patient/info/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import axios from "axios";
+import { Button } from "@/components/ui/button"
 
 
 export default function PatientInfo() {
@@ -31,22 +31,20 @@ export default function PatientInfo() {
         router.push('/');
     }
 
+    // Handle add patient info request. 
+    // Function will be updated when backend is ready.
     async function handleAddPatient(evt: React.FormEvent) {
         evt.preventDefault();
-        console.log('formData=', formData)
-        if (!formData.firstName || !formData.lastName) {
+        const newFormData = { ...formData, patientNum: '' }
+        console.log('newFormData=', newFormData)
+        if (!newFormData.firstName || !newFormData.lastName) {
             setError('add');
             return;
         }
-        try {
-            //example request. add more logic when backend created
-            const result = await axios.post('www.example.com', { data: { ...formData } });
-            const resp = result.data;
-        } catch (err) {
-            console.log('err:', err);
-        }
     }
 
+    // Handle update patient info request. 
+    // Function will be updated when backend is ready.
     async function handleUpdatePatient(evt: React.FormEvent) {
         evt.preventDefault();
         console.log('formData=', formData)
@@ -54,100 +52,121 @@ export default function PatientInfo() {
             setError('update');
             return;
         }
-        try {
-            //example request. add more logic when backend created
-            const result = await axios.patch('www.example.com', { data: { ...formData } });
-            const resp = result.data;
-        } catch (err) {
-            console.log('err:', err);
-        }
     }
 
     return (
-        <div>
-            <h2>Patient Information Screen</h2>
+        <div className='flex flex-col justify-center items-center p-6'>
+            <h2 className='text-2xl text-center font-semibold md:text-3xl my-9'>Add/Update Patient Information</h2>
 
-            <form>
-                <label htmlFor="patientNum">Patient No:</label>
-                <input
-                    type='text'
-                    name='patientNum'
-                    id='patientNum'
-                    value={formData.patientNum}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">First Name</label>
-                <input
-                    type='text'
-                    name='firstName'
-                    id='firstName'
-                    value={formData.firstName}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">Last Name</label>
-                <input
-                    type='text'
-                    name='lastName'
-                    id='lastName'
-                    value={formData.lastName}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">Street</label>
-                <input
-                    type='text'
-                    name='street'
-                    id='street'
-                    value={formData.street}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">City</label>
-                <input
-                    type='text'
-                    name='city'
-                    id='city'
-                    value={formData.city}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">State</label>
-                <input
-                    type='text'
-                    name='state'
-                    id='state'
-                    value={formData.state}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">Country</label>
-                <input
-                    type='text'
-                    name='country'
-                    id='country'
-                    value={formData.country}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">Telephone</label>
-                <input
-                    type='text'
-                    name='telephone'
-                    id='telephone'
-                    value={formData.telephone}
-                    onChange={handleChange}
-                />
-                <label htmlFor="firstName">Contact Email</label>
-                <input
-                    type='text'
-                    name='contactEmail'
-                    id='contactEmail'
-                    value={formData.contactEmail}
-                    onChange={handleChange}
-                />
-
-                <button type='button' onClick={handleCancel}>Cancel</button>
-                <button type='submit' onClick={handleAddPatient}>Add</button>
-                <button type='submit' onClick={handleUpdatePatient}>Update</button>
+            <form className="flex flex-col justify-center">
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="patientNum" className="w-full sm:w-32 text-left font-bold py-1">Patient No:</label>
+                    <input
+                        type='text'
+                        name='patientNum'
+                        id='patientNum'
+                        value={formData.patientNum}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="firstName" className="w-full sm:w-32 text-left font-bold">First Name:</label>
+                    <input
+                        type='text'
+                        name='firstName'
+                        id='firstName'
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="lastName" className="w-full sm:w-32 text-left font-bold">Last Name:</label>
+                    <input
+                        type='text'
+                        name='lastName'
+                        id='lastName'
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="street" className="w-full sm:w-32 text-left font-bold">Street:</label>
+                    <input
+                        type='text'
+                        name='street'
+                        id='street'
+                        value={formData.street}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="city" className="w-full sm:w-32 text-left font-bold">City:</label>
+                    <input
+                        type='text'
+                        name='city'
+                        id='city'
+                        value={formData.city}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="state" className="w-full sm:w-32 text-left font-bold">State:</label>
+                    <input
+                        type='text'
+                        name='state'
+                        id='state'
+                        value={formData.state}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="country" className="w-full sm:w-32 text-left font-bold">Country:</label>
+                    <input
+                        type='text'
+                        name='country'
+                        id='country'
+                        value={formData.country}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="telephone" className="w-full sm:w-32 text-left font-bold">Telephone:</label>
+                    <input
+                        type='text'
+                        name='telephone'
+                        id='telephone'
+                        value={formData.telephone}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
+                    <label htmlFor="contactEmail" className="w-full sm:w-32 text-left font-bold">Contact Email:</label>
+                    <input
+                        type='text'
+                        name='contactEmail'
+                        id='contactEmail'
+                        value={formData.contactEmail}
+                        onChange={handleChange}
+                        className="w-full sm:flex-1 border-2 border-gray-400 rounded-xs px-3 mb-3"
+                    />
+                </div>
+                <div className="flex flex-row justify-center my-8 space-x-4">
+                    <Button className="bg-sky-500 hover:bg-sky-700" type='submit' onClick={handleAddPatient}>Add</Button>
+                    <Button className="bg-sky-500 hover:bg-sky-700" type='submit' onClick={handleUpdatePatient}>Update</Button>
+                    <Button className="bg-sky-500 hover:bg-sky-700" type='button' onClick={handleCancel}>Cancel</Button>
+                </div>
             </form>
 
-            {error === 'add' ? <p>First Name and Last Name are required</p> : null}
-            {error === 'update' ? <p>Patient Number is required</p> : null}
-        </div>
+            {error === 'add' ? <p className="text-red-500 font-bold">First Name and Last Name are required</p> : null}
+            {error === 'update' ? <p className="text-red-500 font-bold">Patient Number is required</p> : null}
+        </div >
     )
 };
