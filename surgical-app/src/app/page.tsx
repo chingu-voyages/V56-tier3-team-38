9b80d3110357ac42adcb8ff184b3b5c07b979c0e
Lisa Chan <lc131715@gmail.com>
2025-08-04 15:29:32 -0700

    update nav links

--- surgical-app/src/app/page.tsx
+++ surgical-app/src/app/page.tsx
@@ -3,11 +3,9 @@ import Header from '@/components/Header';
 
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <>
       {/* TODO: Add landing page content here */}
       <div className="flex justify-center items-center min-h-[85vh]">Contents</div>
-      <Footer />
-    </div>
+    </>
   );
 }
